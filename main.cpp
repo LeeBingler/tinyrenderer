5ee56702341c3cae47ff77e9cc885c8db1c431ce
Lee <binglerlee@gmail.com>
2026-07-24 10:12:31 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include "model.h"
+#include "tgaimage.h"
+#include <cmath>
+#include <utility>
+
+constexpr TGAColor white = {255, 255, 255, 255}; // attention, BGRA order
+constexpr TGAColor green = {0, 255, 0, 255};
+constexpr TGAColor red = {0, 0, 255, 255};
+constexpr TGAColor blue = {255, 128, 64, 255};
+constexpr TGAColor yellow = {0, 200, 255, 255};
+
+void line(int ax, int ay, int bx, int by, TGAImage &framebuf, TGAColor color) {
+  bool steep = std::abs(ax - bx) < std::abs(ay - by);
+  if (steep) { // if the line is steep, we transpose the image
+    std::swap(ax, ay);
+    std::swap(bx, by);
+  }
+  if (ax > bx) {
+    std::swap(ax, bx);
+    std::swap(ay, by);
+  }
+
+  int y = ay;
+  int ierror = 0;
+
+  for (int x = ax; x <= bx; x++) {
+    if (steep) // if transposed, de−transpose
+      framebuf.set(y, x, color);
+    else
+      framebuf.set(x, y, color);
+
+    ierror += 2 * std::abs(by - ay);
+    y += (by > ay ? 1 : -1) * (ierror > bx - ax);
+    ierror -= 2 * (bx - ax) * (ierror > bx - ax);
+  }
+}
+
+int main(int argc, char **argv) {
+  constexpr int width = 800;
+  constexpr int height = 800;
+  TGAImage framebuffer(width, height, TGAImage::RGB);
+
+  /*
+  int ax = 7, ay = 3;
+  int bx = 12, by = 37;
+  int cx = 62, cy = 53;
+
+  line(ax, ay, bx, by, framebuffer, blue);
+  line(cx, cy, bx, by, framebuffer, green);
+  line(cx, cy, ax, ay, framebuffer, yellow);
+  line(ax, ay, cx, cy, framebuffer, red);
+
+  framebuffer.set(ax, ay, white);
+  framebuffer.set(bx, by, white);
+  framebuffer.set(cx, cy, white);
+
+  framebuffer.write_tga_file("framebuffer.tga");
+  */
+
+  Model m(argv[1]);
+  m.load();
+
+  return 0;
+}
---
+++ model.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+class Model {
+public:
+  std::string path;
+  Model(std::string path) : path(path) {};
+  ~Model() {};
+
+  int load() {
+    std::string line;
+    std::ifstream file(this->path);
+
+    std::getline(file, line);
+
+    std::cout << this->path << std::endl;
+    std::cout << line << std::endl;
+
+    return 0;
+  };
+
+  int unload() { return 0; };
+};
